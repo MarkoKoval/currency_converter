b50415be59a398e18bb2aead220da780ee3a9c35
MarkoKoval <mark.koval.ua@gmail.com>
2021-03-01 16:47:26 +0200

    reformat code using flake8

--- currencies.py
+++ currencies.py
@@ -2,18 +2,30 @@ import requests
 from datetime import datetime
 import json
 
+api_source = "https://bank.gov.ua/NBUStatService/v1" \
+             "/statdirectory/exchange?date={}&json"
+
+
+def get_currency_rates():
+    """extract currency rates for current day using API"""
+
+    r = requests.get(api_source.format(
+        "".join(str(datetime.now()).split()[0].split('-'))))
+    return {item["cc"]: (item["txt"], item["rate"]) for item
+            in json.loads(r.text)}
+
 
 class Ccy:
     """Currency convertor class"""
 
     # extract currency rates from API
-    currency_rates = {item["cc"]: (item["txt"], item["rate"]) for item in json.loads(requests.get(
-        "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date={}&json".format(
-            "".join(str(datetime.now()).split()[0].split('-')))).text)}
+    currency_rates = get_currency_rates()
 
     def __init__(self, amount, currency):
 
-        if not isinstance(currency, str) or (currency not in self.currency_rates and currency != "UAH") or not isinstance(amount, float) or amount < 0:
+        if not isinstance(currency, str) or \
+                (currency not in self.currency_rates and currency != "UAH")\
+                or not isinstance(amount, float) or amount < 0:
             raise Exception("Enter correct data")
 
         self._amount = amount
@@ -35,7 +47,8 @@ class Ccy:
 
     @currency.setter
     def currency(self, name):
-        if not isinstance(name, str) or (name not in self.currency_rates and name != "UAH"):
+        if not isinstance(name, str) or \
+                (name not in self.currency_rates and name != "UAH"):
             raise Exception("Enter correct data")
         self._currency = name
 
@@ -49,7 +62,8 @@ class Ccy:
         elif secondary_currency == "UAH":
             return 1.0 / self.currency_rates[base_currency][1]
         else:
-            return self.currency_rates[secondary_currency][1] / self.currency_rates[base_currency][1]
+            return self.currency_rates[secondary_currency][1]\
+                   / self.currency_rates[base_currency][1]
 
     def find_rate(self, amount, base_currency, secondary_currency):
         return amount * self.get_rate(base_currency, secondary_currency)
@@ -59,26 +73,35 @@ class Ccy:
         if self.currency == secondary.currency:
             return Ccy(self.amount + secondary.amount, self.currency)
         elif self.currency != secondary.currency and (
-                (self.currency in self.currency_rates and secondary.currency in self.currency_rates) or
-                (self.currency == "UAH" and secondary.currency in self.currency_rates) or
-                (self.currency in self.currency_rates and secondary.currency == "UAH")):
-            return Ccy(self.amount + self.find_rate(secondary.amount, self.currency, secondary.currency), self.currency)
+                (self.currency in self.currency_rates
+                 and secondary.currency in self.currency_rates) or
+                (self.currency == "UAH" and
+                 secondary.currency in self.currency_rates) or
+                (self.currency in self.currency_rates
+                 and secondary.currency == "UAH")):
+            return Ccy(self.amount + self.find_rate(
+                secondary.amount, self.currency,
+                secondary.currency), self.currency)
         else:
             raise Exception("Enter correct data")
 
     def __sub__(self, secondary):
         """ subtract converted secondary currency from base"""
-        if self.currency == secondary.currency and self.amount > secondary.amount:
+        if self.currency == secondary.currency \
+                and self.amount > secondary.amount:
             return Ccy(self.amount - secondary.amount, self.currency)
         elif self.currency != secondary.currency and (
-                (self.currency in self.currency_rates and secondary.currency in self.currency_rates) or
-                (self.currency == "UAH" and secondary.currency in self.currency_rates) or
-                (self.currency in self.currency_rates and secondary.currency == "UAH")):
-            converted = self.find_rate(secondary.amount, self.currency, secondary.currency)
+                (self.currency in self.currency_rates and
+                 secondary.currency in self.currency_rates) or
+                (self.currency == "UAH" and
+                 secondary.currency in self.currency_rates) or
+                (self.currency in self.currency_rates
+                 and secondary.currency == "UAH")):
+            converted = self.find_rate(secondary.amount,
+                                       self.currency, secondary.currency)
 
             if converted > self.amount:
                 Exception("Enter correct data")
             return Ccy(self.amount - converted, self.currency)
         else:
             raise Exception("Enter correct data")
-
--- run.py
+++ run.py
@@ -13,17 +13,22 @@ while True:
                 else:
                     print(it + " " + str(Ccy.currency_rates[it]), end=" ")
 
-            base_curr_identifier = input("Enter base currency identifier (UAH, USD ....) : ")
-            base_curr_amount = float(input("Enter base currency amount (1, 1.2 ...) : "))
+            base_curr_identifier = input("Enter base "
+                                         "currency identifier (UAH,USD ..) : ")
+            base_curr_amount = float(input("Enter base"
+                                           " currency amount (1, 1.2 ...) : "))
 
             base = Ccy(base_curr_amount, base_curr_identifier)
             print(base)
-            added_curr_identifier = input("Enter added currency identifier (UAH, USD ....) : ")
-            added_curr_amount = float(input("Enter added currency amount (1, 1.2 ...) : "))
+            added_curr_identifier = input("Enter added currency"
+                                          " identifier (UAH, USD ....) : ")
+            added_curr_amount = float(input("Enter added currency "
+                                            "amount (1, 1.2 ...) : "))
 
             added = Ccy(added_curr_amount, added_curr_identifier)
             print(added)
-            operation = input("Select choice operation type + or - between currencies: ")
+            operation = input("Select choice operation "
+                              "type + or - between currencies: ")
             if operation == "+":
                 print(base + added)
             elif operation == "-":
